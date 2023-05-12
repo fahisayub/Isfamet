b02c386864bf8d40c542d06b30cff4f1ad3c8a30
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2023-05-12 12:31:23 +0530

    project setup

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/page.module.css'
+
+export default function Profile(){
+    return <main className={styles.main}>
+      <div className={styles.description}>
+        Hai
+      </div>
+    </main>
+}
